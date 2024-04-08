4dff73185660fa745c6448e9ca65b34980603fb4
Ahsan99r <165573133+Ahsan99r@users.noreply.github.com>
2024-04-08 19:24:05 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,63 @@
+import inquirer from"inquirer";
+import Choice from "inquirer/lib/objects/choice.js";
+
+let myBalance = 50000; // Dollar.
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt(
+    [
+    {
+        name: "pin",
+        message: "Please enter your pincode",
+        type: "number",
+    },
+
+    ]
+);
+
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code!!!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Please select an option",
+            type: "list",
+            choices: ["withdraw", "check balance"],
+        }
+    ]
+);
+console.log(operationAns);
+if(operationAns.operation === "withdraw"){
+    let amountAns = await inquirer.prompt(
+        [
+           {
+            name: "amount",
+            message: "enter your amount",
+            type:"number"
+           },
+        ]
+    );
+    myBalance -=amountAns.amount;
+    console.log("YOur remaining balance is:" + myBalance)
+
+}else if(operationAns.operation ==="check balance")
+    {
+        console.log("your balance is:" + myBalance)
+    }
+
+
+
+
+
+
+
+
+}
+else{
+    console.log("Incorrect pin number");
+}
+
+
+
+
+
+
